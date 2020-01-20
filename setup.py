e7d9bc2aeeddb82d82abe79745a98f8ccf07e1ea
ykatsu111 <41444410+ykatsu111@users.noreply.github.com>
2020-01-20 21:26:12 +0900

    under const

--- setup.py
+++ setup.py
@@ -6,28 +6,46 @@ import os
 
 
 __version__ = "0.1"
+__author__ = "ykatsu111"
+__description__ = "A fitting algorithm for PSVD data with the EM algorithm."
+__long_description__ = ""
 
 
-ext_modules = [
-    Extension(
-        "pyempsvd",
-        ["cpp/EmpsvdCore.cpp", "cpp/PyEmpsvd.cpp"],
-        include_dirs=[
-            pybind11.get_include(),
-            pybind11.get_include(True)
-        ],
-        language="c++",
-        requires=["pybind11"],
-        extra_compile_args=["-std=c++11"]
+if os.environ("ENABLE_CXX") == "OFF":
+
+    setup(
+        name="pyempsvd",
+        version=__version__,
+        author=__author__,
+        description=__description__,
+        long_description=__long_description__,
+        package_dir={"": "python"},
+        py_modules=["pyempsvd"],
+        requires=["numpy", "scipy"]
     )
-]
 
-setup(
-    name="pyempsvd",
-    version=__version__,
-    author="ykatsu111",
-    description="A fitting algorithm for PSVD data with the EM algorithm.",
-    long_description="",
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext}
-)
+else:
+
+    ext_modules = [
+        Extension(
+            "pyempsvd",
+            ["cpp/EmpsvdCore.cpp", "cpp/PyEmpsvd.cpp"],
+            include_dirs=[
+                pybind11.get_include(),
+                pybind11.get_include(True)
+            ],
+            language="c++",
+            requires=["pybind11"],
+            extra_compile_args=["-std=c++11"]
+        )
+    ]
+
+    setup(
+        name="pyempsvd",
+        version=__version__,
+        author=__author__,
+        description=__description__,
+        long_description=__long_description__,
+        ext_modules=ext_modules,
+        cmdclass={"build_ext": build_ext}
+    )
